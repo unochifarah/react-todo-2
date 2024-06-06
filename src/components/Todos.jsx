7c935ee0ae09d0d461a34fedc5e1934c596890f5
Jasmine Unochi <jasmineunochii@gmail.com>
2024-06-06 15:47:44 +0700

    finished

---
+++ src/components/Todos.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import TodoItem from './TodoItem'
+
+const Todos = ({ todos, toggleCompleted, deleteTodo }) => {
+  return(
+    <div style={styles.container}>
+       {todos.map((todo) => {
+        // Berikan datanya ke TodoItem
+          return (
+            <TodoItem 
+                key={todo.id} 
+                todo={todo} 
+                toggleCompleted={toggleCompleted}
+                deleteTodo={deleteTodo}
+            />
+          ) 
+      })}
+    </div>
+  )
+}
+
+const styles = {
+  container: {
+    width: '40%',
+    margin: '0 auto',
+  },
+}
+
+export default Todos;
